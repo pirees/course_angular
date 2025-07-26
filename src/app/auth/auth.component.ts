695b4f77285c233d408f9fd037a5911d1b105b0a
Leonardo Pires <leonardopires-1997@hotmail.com>
2025-07-26 16:49:42 -0300

    638 class

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, ViewChild, ViewContainerRef } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthResponseData, AuthService } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -43,12 +48,37 @@ export class AuthComponent {
       },
       errorMessage => {
         console.log(errorMessage);
-        this.error = errorMessage
+        //this.error = errorMessage
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     );
 
     form.reset();
   }
+
+  onHandleError(){
+    this.error = null;
+  }
+
+  ngOnDestroy(): void {
+    if(this.closeSub){
+      this.closeSub.unsubscribe();
+    }
+  }
+
+  private showErrorAlert(message: string){
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(AlertComponent);
+
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
+
 
 }
